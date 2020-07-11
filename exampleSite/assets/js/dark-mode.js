f97e5fb508e0b96bac102ffbeb9ccd8062ac311f
Clément Pannetier <clement.pannetier49@gmail.com>
2020-07-11 23:19:34 +0200

    Added back colorsheme configurations + script moved to be process by hugo pipe

--- exampleSite/assets/js/dark-mode.js
+++ exampleSite/assets/js/dark-mode.js
@@ -2,9 +2,13 @@ const body = document.body;
 const darkModeToggle = document.getElementById('dark-mode-toggle');
 const darkModeMediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
 
+// Check if user preference is set, if not check value of body class for light or dark else it means that colorsheme = auto
 if (localStorage.getItem("colorscheme")) {
   setTheme(localStorage.getItem("colorscheme"));
+} else if (body.classList.contains('colorscheme-light') || body.classList.contains('colorscheme-dark')) {
+  setTheme(body.classList.contains("colorscheme-dark") ? "dark" : "light");
 } else {
+  body.classList.remove('colorscheme-auto');
   setTheme(darkModeMediaQuery.matches ? "dark" : "light");
 }
 
