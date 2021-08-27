202c6b8ca9af404d3dacd6b2f67667bb11bd0c34
kuba86 <kuba86@gmail.com>
2021-08-28 00:38:13 +0200

    moved counter.dev to js file to avid minify

--- assets/js/coder.js
+++ assets/js/coder.js
@@ -38,3 +38,5 @@ function setTheme(theme) {
 function rememberTheme(theme) {
     localStorage.setItem('colorscheme', theme);
 }
+
+if(!sessionStorage.getItem("_swa")&&document.referrer.indexOf(location.protocol+"//"+location.host)!== 0){fetch("https://counter.dev/track?"+new URLSearchParams({referrer:document.referrer,screen:screen.width+"x"+screen.height,user:"kuba86",utcoffset:"2"}))};sessionStorage.setItem("_swa","1");
